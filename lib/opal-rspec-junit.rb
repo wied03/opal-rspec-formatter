7d222b5e040e51ca52e9b25be199efbb5a9963e9
Brady Wied <brady@wied.us>
2015-06-10 22:51:29 -0600

    Remove hard coded path

--- lib/opal-rspec-junit.rb
+++ lib/opal-rspec-junit.rb
@@ -3,4 +3,5 @@ require 'opal-builder'
 
 Opal.append_path File.expand_path('../../opal', __FILE__).untaint
 # Not using use_gem because we don't want the RSpec dependencies rspec_junit_formatter has
-Opal.append_path '/usr/local/bundle/gems/rspec_junit_formatter-0.2.3/lib'
+junit = Gem::Specification.find_by_name('rspec_junit_formatter')
+Opal.append_path File.join(junit.gem_dir, 'lib')
