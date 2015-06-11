d752d3223858c937d16cac29693123c9f36e29a9
Brady Wied <brady@bswtechconsulting.com>
2015-06-11 14:35:29 -0600

    Replicate issue with running from Rubymine

---
+++ spec_mri/teamcity_spec.rb
@@ -0,0 +1,30 @@
+describe 'TeamCity' do
+  let(:command) {
+    "RUBYLIB='../teamcity/patch/common:../teamcity/patch/bdd' SPEC_OPTS='#{spec_opts}' rake raw_specs"
+  }
+  
+  subject {
+    `#{command}`
+  }
+  
+  RSpec.shared_context :example do
+    it { is_expected.to match /##teamcity\[testSuiteFinished/ }
+  end
+  
+  context 'normal run' do
+    let(:spec_opts) {
+      '--append_exp_from_load_path patch/bdd --append_exp_from_load_path patch/common --require opal/rspec/formatters/teamcity --format Opal::RSpec::Formatters::TeamCity'
+    }
+    
+    include_context :example
+  end
+  
+  context 'TC tries to override the formatter' do
+    # TeamCity will try and add its own contents to SPEC_OPTS    
+    let(:spec_opts) {
+      '--append_exp_from_load_path patch/bdd --append_exp_from_load_path patch/common --require opal/rspec/formatters/teamcity --format Opal::RSpec::Formatters::TeamCity --require teamcity/spec/runner/formatter/teamcity/formatter --format Spec::Runner::Formatter::TeamcityFormatter'
+    }
+    
+    include_context :example
+  end
+end
