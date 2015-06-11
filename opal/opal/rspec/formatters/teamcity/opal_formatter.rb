a52f35da0eeca272af1d18dc5e75253c548a3fb6
Brady Wied <brady@bswtechconsulting.com>
2015-06-11 11:31:20 -0600

    Deal with fluent examples better

--- opal/opal/rspec/formatters/teamcity/opal_formatter.rb
+++ opal/opal/rspec/formatters/teamcity/opal_formatter.rb
@@ -13,9 +13,34 @@ module Opal
                                            :example_pending, :example_failed,
                                            :dump_summary, :seed
                                            
+       # TeamCity doesn't yet identify fluent style examples. In Javascript/Opal, the effect of this is even more pronounced
+       # since filenames and line numbers do not mean as much, so waiting to tell TeamCity the example started until it's finished,
+       # that way we know the full description - https://youtrack.jetbrains.com/issue/RUBY-15519
+       
+       alias_method :orig_example_started, :example_started
+       
+       def example_started(notification)
+       end
+       
        def dump_summary(notification)
          super
+         # Otherwise we won't know when to exit
          finish_phantom notification
+       end
+       
+       def example_passed(notification)
+         orig_example_started notification
+         super
+       end
+       
+       def example_pending(notification)
+         orig_example_started notification
+         super
+       end
+       
+       def example_failed(notification)
+         orig_example_started notification
+         super
        end
       end
     end
