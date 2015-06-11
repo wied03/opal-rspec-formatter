1de0b5903aeb41e82c1bff3463cb5feb5ece62f4
Brady Wied <brady@bswtechconsulting.com>
2015-06-11 11:31:01 -0600

    Disable debug now that things are working

--- opal/opal/rspec/formatters/teamcity/formatter.rb
+++ opal/opal/rspec/formatters/teamcity/formatter.rb
@@ -2,4 +2,4 @@ require 'opal/rspec/formatters/teamcity/debug_logger'
 
 # Since we didn't use the main file, we need to add some missing dependencies
 # We can't log to files
-SPEC_FORMATTER_LOG = ::Rake::TeamCity::Utils::ConsoleLogger.new(enabled=true)
+SPEC_FORMATTER_LOG = ::Rake::TeamCity::Utils::ConsoleLogger.new(enabled=false)
