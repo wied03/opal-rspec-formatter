117f20ffe94a9fd89fa301a0255a3dbc5e540cf4
Brady Wied <brady@bswtechconsulting.com>
2015-06-11 11:10:37 -0600

    Still need this

--- opal/opal/rspec/formatters/teamcity/std_capture_helper.rb
+++ opal/opal/rspec/formatters/teamcity/std_capture_helper.rb
@@ -4,4 +4,15 @@ module Rake::TeamCity::StdCaptureHelper
   def isCaptureDisabled()
     true
   end
+  
+  # flush is not supported on STDOUT or STDERR and it was at the beginning of this method
+  def capture_output_end_external(old_out, old_err, new_out, new_err)
+    if isCaptureDisabled()
+      return "", ""
+    end
+
+    reopen_stdout_stderr(old_out, old_err)
+
+    return get_redirected_stdout_stderr_from_files(new_out, new_err)
+  end  
 end
