c4a55ca08fd2d0f0e62cabc4f91dbbb160e02fa3
Brady Wied <brady@bswtechconsulting.com>
2015-10-19 21:26:48 -0600

    Remove unnecessary runner coupling and bump version

--- lib/opal/rspec-formatter/version.rb
+++ lib/opal/rspec-formatter/version.rb
@@ -1,7 +1,7 @@
 module Opal
   module RSpec
     module Formatter
-      VERSION = '1.0.1'
+      VERSION = '1.1.0'
     end
   end
 end
--- opal/opal/rspec/formatters/junit.rb
+++ opal/opal/rspec/formatters/junit.rb
@@ -1,43 +1,40 @@
 require 'rspec_junit_formatter'
-require 'opal/rspec/formatters/phantom_util'
 
 module Opal
   module RSpec
     module Formatters
       class Junit < ::RSpecJUnitFormatter
-        include Opal::RSpec::Formatters::PhantomUtil
         ::RSpec::Core::Formatters.register self,
-            :start,
-            :stop,
-            :dump_summary
-            
+                                           :start,
+                                           :stop,
+                                           :dump_summary
+
         def initialize
           # with console output, new lines will be all over the place
           super output=StringIO.new
         end
-      
+
         def dump_summary(notification)
           @summary_notification = notification
           xml_dump
           puts '---begin xml---'
           puts output.string
           puts '---end xml---'
-          finish_phantom notification
-        end     
+        end
 
         # class name based on filename is not that meaningful in the JS world
         def classname_for(notification)
           group = notification.example.example_group
           # Don't need to show the top level/example group
-          show = group.parent_groups.reject {|g| g == ::RSpec::Core::ExampleGroup}
-          show.reverse.map {|g| g.description}.join '::'
+          show = group.parent_groups.reject { |g| g == ::RSpec::Core::ExampleGroup }
+          show.reverse.map { |g| g.description }.join '::'
         end
-        
+
         # Since we include most of the description under classname, only need stuff for this example here
         def description_for(notification)
           notification.example.description
-        end        
+        end
       end
-    end  
+    end
   end
 end
